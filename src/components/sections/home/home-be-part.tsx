db50aafc81c0d2278230e48acb3b8c332d80ad8f
Batyr <awbatyr@gmail.com>
2025-11-23 15:45:35 +0500

    added photos

--- src/components/sections/home/home-be-part.tsx
+++ src/components/sections/home/home-be-part.tsx
@@ -10,13 +10,15 @@ export const HomeBePart = () => {
     cards: CardType[];
   };
 
+  const images = ["/home/be-part/1.jpg", "/home/be-part/2.jpg"];
+
   return (
     <section className="container flex flex-col gap-10">
       <h2 className="h2 text-center">{data.title}</h2>
 
       <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
         {data.cards.map((item, i) => (
-          <Card key={i} img="/home/be-part/1.jpg" {...item} />
+          <Card key={i} img={images[i]} {...item} />
         ))}
       </div>
     </section>
